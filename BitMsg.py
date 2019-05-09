f9783dc254311ce6abae1d75e8f5e205ca6e6c6c
Ofir <ofir1080@gmail.com>
2019-05-09 22:37:52 +0300

    fixed inject.set_pixel

--- BitMsg.py
+++ BitMsg.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class BitMsg:
     def __init__(self, msg):
-        char_list = [bin(ord(c))[2:] for c in msg]  # from char to decimal
+        char_list = [bin(ord(c))[2:].zfill(8) for c in msg]  # from char to decimal
         bit_list = ''.join(char_list)               # from decimal to binary
         self.bit_list = np.array([int(b) for b in bit_list])  # from binary to bits
         self.size = len(self.bit_list)
--- inject.py
+++ inject.py
@@ -1,7 +1,7 @@
-#
-# def convert_str_to_bin(msg):
-#     binary_list = [bin(ord(c))[2:] for c in msg]
-#     return ''.join(binary_list)
+"""
+injects message into the carrier matrix represents the image.
+"""
+import numpy as np
 
 
 def inject(carrier, msg):
@@ -11,12 +11,13 @@ def inject(carrier, msg):
             if bit_counter >= msg.size:
                 # run as long as there are bits to inject
                 return
-            print(carrier.mat[row][col])
             carrier.mat[row][col] = set_pixel(carrier.mat[row][col], msg.bit_list[bit_counter:bit_counter + 3])
             bit_counter += 3
 
 
 def set_pixel(pixel, bits):
-    # new_pixel = (pixel[:bits.size] & 0b01111111) | (bits << 7)    # uncomment to set MSBs
-    new_pixel = (pixel[:bits.size] & 0b11111110) | bits
+    while bits.size < 3:
+        bits = np.append(bits, 1)
+    new_pixel = (pixel & 0b11111110) | bits
+    # new_pixel = (pixel & 0b01111111) | bits << 7  # uncomment for setting MSBs
     return new_pixel
