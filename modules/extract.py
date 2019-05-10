f2103d94f57454127c9bcdc108c4f00a869bda78
Ofir <ofir1080@gmail.com>
2019-05-11 01:30:51 +0300

    changed 'extract' module logic'. reading from image now is byte by byte (not pixel by pixel)

--- modules/extract.py
+++ modules/extract.py
@@ -5,53 +5,51 @@ length of the message in bits (k * 8 where k is the number of characters.
 import numpy as np
 
 
-def extract_msg(carrier, length):
-    extracted_bits = extract_LSB(carrier, length)
-    msg = convert_bits_to_txt(extracted_bits)
+def extract_msg(carrier):
+    extracted_bytes = extract_bytes(carrier)
+    msg = convert_to_txt(extracted_bytes)
     return msg
 
 
-def extract_LSB(carrier, msg_length):
+def extract_bytes(carrier):
     """
-
     :param carrier: obj of type Carrier representing the carrier image
-    :param msg_length: the length of the message in bits (where each character is of size 8)
-    :return: a list of bits of the message
+    :return: extracted message (string)
     """
-    bit_list = np.array([])
+    byte_val = ''
+    ascii_list = []
     bit_counter = 0
     for row in range(carrier.height):
         for col in range(carrier.width):
-            if bit_counter >= msg_length:
-                # if all bits needed already extracted
-                return bit_list
-            bit_list = np.append(bit_list, extract_LSB_from_RGB(carrier.mat[row][col], min(3, msg_length - bit_counter)))
-            bit_counter += 3
+            for c in range(3):
+                byte_val = byte_val + str(extract_LSB(carrier.mat[row][col][c]))
+                bit_counter += 1
+                if bit_counter == 8:
+                    decimal_val = int(byte_val, 2)
+                    if is_msg_end(decimal_val):
+                        return ascii_list
+                    else:
+                        ascii_list.append(decimal_val)
+                        byte_val = ''
+                        bit_counter = 0
 
 
-def extract_LSB_from_RGB(pixel, bits_in_pixel):
+def is_msg_end(byte):
+    return byte == ord('ÿ')
+
+
+def extract_LSB(num):
     """
-    return a list of the required LSBs (max 3)
-    :param pixel: list of size 3 (RGB type)
-    :param bits_in_pixel: how many LSbits need to be extracted (1, 2 or 3)
-    :return: list of max-3 size containing 0s/1s (bits of the injected message)
+    :param num: decimal 8-bit integer
+    :return: lsb
     """
-    bits = []
-    for i in range(bits_in_pixel):
-        bits.append(bin(pixel[i])[-1])
-    return bits
+    return 0 if num % 2 == 0 else 1
 
 
-def convert_bits_to_txt(bit_list):
+def convert_to_txt(ascii_list):
     """
-
-    :param bit_list: list of chars ('0' of '1') of size k*8 representing bytes of ascii chars
-    :return: the extracted text
+    :param ascii_list: list of 8-bit integers representing ascii characters
+    :return: extracted message (type string)
     """
-    char_list = np.array([])
-    for i in range(0, bit_list.size, 8):
-        # iterates every 8th bit
-        byte = int(''.join(bit_list[i:i + 8]), 2)   # converts every 8 bits to base-2 number
-        char_list = np.append(char_list, chr(byte)) # convert byte to char using ASCII and appends to list of chars
+    char_list = list(map(chr, ascii_list))
     return ''.join(char_list)
-
